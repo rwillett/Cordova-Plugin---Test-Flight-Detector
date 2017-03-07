b3325d8f2840296928f9f913848e8a11d89c6b78
Rob Willett <rob.github@robertwillett.com>
2017-03-07 17:52:20 +0000

    Update XCDetect.js

--- www/XCDetect.js
+++ www/XCDetect.js
@@ -55,7 +55,7 @@ function XCDetect() {
 }
 
 /**
- * TFDetect detect
+ * XCDetect detect
  *
  * @param {Function} successCallback The function to call when the heading data is available
  * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
